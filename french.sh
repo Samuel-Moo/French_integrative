844cbe84a9117889fa9adc527126a24991ce964e
Samuel_Moo <samuelmoo05@gmail.com>
2022-12-03 17:52:10 -0600

    try at a array

--- french.sh
+++ french.sh
@@ -6,7 +6,7 @@ echo "Bienvenido al traductor Francés-Español!"
 echo "Inserte la frase a traducir aquí"
 
 #----LEEMOS INPUT----#
-read frase
+read -a arr frase
 
 #----FOR LOOP PARA OBTENER CADA PALABRA INDIVIDUALMENTE----#
 for word in $frase 
@@ -14,31 +14,36 @@ do
     #----SWITCH CASE----#
     case $word in 
         j\'ai) 
-        case $word in 
-            voulu)
-            echo "Sujet: Je, Conjugaison: Passé composé Avoir Verbe:Vouloir"
-            break
-            ;;
-            pu)
-            echo "Sujet: Je, Conjugaison: Passé composé Avoir Verbe:Puvoir"
-            break
-            ;;
-        esac       
+            case $word in 
+                voulu)
+                echo "Sujet: Je, Conjugaison: Passé composé Avoir Verbe:Vouloir"
+                break
+                ;;
+                pu)
+                echo "Sujet: Je, Conjugaison: Passé composé Avoir   Verbe:Puvoir"
+                break
+                ;;
+                *)
+                echo "no te dio mijo"
+                break
+                ;;
+            esac       
         ;;
         as)
-        case $word in 
-            voulais)
-            echo "Sujet: Tu, Conjugaison: Passé composé AvoirVerbe:Vouloir"
-            break
-            ;;
-            pu)
-            echo "Sujet: Tu, Conjugaison: Passé composé Avoir Verbe:Puvoir"
-            break
-            ;;
-        esac
+            case $word in 
+                voulais)
+                echo "Sujet: Tu, Conjugaison: Passé composé     AvoirVerbe:Vouloir"
+                break
+                ;;
+                pu)
+                echo "Sujet: Tu, Conjugaison: Passé composé Avoir   Verbe:Puvoir"
+                break
+                ;;
+            esac
         ;;     
         *)
-        #echo "Subete al EVA shinji"
+        echo "Subete al EVA shinji"
+        break
         ;;
     esac
 done
