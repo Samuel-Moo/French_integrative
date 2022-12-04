bc220069a8879d967df8b3568cac8cfac2f9246b
Samuel_Moo <samuelmoo05@gmail.com>
2022-12-04 16:55:40 -0600

    Passé composé finished

--- french.sh
+++ french.sh
@@ -1,21 +1,138 @@
 #!/bin/bash
 
+#----LIMPIAR----#
 clear
 
-#----IMPRIMIR BIENVENIDA----#
-echo "Bienvenido al traductor Francés-Español!"
-echo "Inserte la frase a traducir aquí"
+#----Functions----#
 
-#----LEEMOS INPUT----#
-read -a frase
+function case_passe_compose {
+    case "${frase[counter]}" in
+        voulu)
+        echo " Verbe:Vouloir"
+        exit
+        ;;
+        pu)
+        echo " Verbe:Puvoir"
+        exit
+        ;;
+        dû)
+        echo " Verbe: Devoir"
+        exit
+        ;;
+        été)
+        echo " Verbe: Être"
+        exit
+        ;;
+        eu)
+        echo " Verbe: Avoir"
+        exit
+        ;;
+        aimé)
+        echo " Verber: Aimer"
+        exit
+        ;;
+        changé)
+        echo " Verbe: Changer"
+        exit
+        ;;
+        mangé)
+        echo " Verbe: Manger"
+        exit
+        ;;
+        parlé)
+        echo " Verbe: Parler"
+        exit
+        ;;
+        *)
+        clear
+        echo "Pas de coïncidences"
+        exit
+        ;;
+    esac
+}
+function case_passe_compose_etre {
+    case "${frase[counter]}" in
+        allé)
+        echo " Verbe: Aller"
+        exit
+        ;;
+        resté)
+        echo " Verbe: Rester"
+        exit
+        ;;
+        *)
+        clear
+        echo "Pas de coïncidences"
+        exit
+        ;;
+    esac
+}
 
-# for i in "${frase[@]}"
-# do 
-#     echo $i
-# done
+function jai {
+    echo -n "Sujet: Je, Conjugaison: Passé composé avoir"
+    case_passe_compose
+}
 
-counter=0
-#----FOR LOOP PARA OBTENER CADA PALABRA INDIVIDUALMENTE----#
+function as {
+    echo -n "Sujet: Tu, Conjugaison: Passé composé avoir"
+    case_passe_compose
+}
+
+function a {
+    echo -n "Sujet: Il/Elle/On, Conjugaison: Passé composé avoir"
+    case_passe_compose
+}
+
+function avons {
+    echo -n "Sujet: Nous, Conjugaison: Passé composé avoir"
+    case_passe_compose
+}
+
+function avez {
+    echo -n "Sujet: Vous, Conjugaison: Passé composé avoir"
+    case_passe_compose
+}
+
+function ont {
+    echo -n "Sujet: Ils/Elles, Conjugaison: Passé composé avoir"
+    case_passe_compose
+}
+
+function suis {
+    echo -n "Sujet: Je, Conjugaison: Passé composé Être"
+    case_passe_compose_etre
+}
+function es {
+    echo -n "Sujet: Tu, Conjugaison: Passé composé Être"
+    case_passe_compose_etre
+}
+
+function est {
+    echo -n "Sujet: Il/Elle/On, Conjugaison: Passé composé Être"
+    case_passe_compose_etre
+}
+
+function sommes {
+    echo -n "Sujet: Nous, Conjugaison: Passé composé Être"
+    case_passe_compose_etre
+}
+
+function etes {
+    echo -n "Sujet: Vous, Conjugaison: Passé composé Être"
+    case_passe_compose_etre
+}
+
+function sont {
+    echo -n "Sujet: Ils/Elles, Conjugaison: Passé composé Être"
+    case_passe_compose_etre
+}
+
+function imparfait {
+    echo "a"
+}
+
+function main {
+    #----FOR LOOP PARA OBTENER CADA PALABRA INDIVIDUALMENTE----#
 for word in "${frase[@]}"
 do
     #----Counter----#
@@ -24,36 +141,65 @@ do
     #----SWITCH CASE----#
     case $word in 
         j\'ai) 
-            case "${frase[counter]}" in 
-                voulu)
-                echo "Sujet: Je, Conjugaison: Passé composé Avoir Verbe:Vouloir"
-                break
-                ;;
-                pu)
-                echo "Sujet: Je, Conjugaison: Passé composé Avoir   Verbe:Puvoir"
-                break
-                ;;
-                *)
-                echo "no te dio mijo"
-                break
-                ;;
-            esac       
+        jai     
         ;;
         as)
-            case "${frase[counter]}" in 
-                voulais)
-                echo "Sujet: Tu, Conjugaison: Passé composé Avoir Verbe:Vouloir"
-                break
-                ;;
-                pu)
-                echo "Sujet: Tu, Conjugaison: Passé composé Avoir   Verbe:Puvoir"
-                break
-                ;;
-            esac
-        ;;     
+        as
+        ;;
+        a)
+        a
+        ;;
+        avons)
+        avons
+        ;;
+        avez)
+        avez
+        ;;
+        ont)
+        ont
+        ;;
+        suis)
+        suis
+        ;;
+        es)
+        es
+        ;;
+        est)
+        est
+        ;;
+        sommes)
+        sommes
+        ;;
+        etes)
+        etes
+        ;;
+        sont)
+        sont
+        ;;
         *)
+        imparfait
         ;;
     esac
 done
+}
 
-#echo $counter
+#----IMPRIMIR BIENVENIDA----#
+echo "Bienvenido al traductor Francés-Español!"
+echo "Inserte la frase a traducir aquí"
+
+#----LEEMOS INPUT----#
+read -a frase
+
+#----Convertimos Todo Minusculas----#
+frase=( "${frase[@],,}" )
+
+# for i in "${frase[@]}"
+# do 
+#     echo $i
+# done
+
+#----CONTADOR----#
+counter=0
+
+#----Ejecutar codiguillo----#
+main
